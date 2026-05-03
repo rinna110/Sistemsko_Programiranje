7c846319acaf451d7dc4739372f55893075c0229
katarinaducic <katarina.ducic@elfak.rs>
2026-05-03 22:38:34 +0200

    mala promena

--- zad8/Program.cs
+++ zad8/Program.cs
@@ -2,5 +2,7 @@
 const int max_connections = 100;
 
 HttpServer server = new HttpServer(server_port, max_connections);
-
+Logger.Log("Starting HTTP server...");
+Console.WriteLine("Server running on port " + server_port);
+Console.WriteLine("Max connections:"+ max_connections);
 server.listen();
