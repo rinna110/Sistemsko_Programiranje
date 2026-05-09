b29c9d33a3f096f2994fbab79f100b937bd48492
katarinaducic <katarina.ducic@elfak.rs>
2026-05-09 14:24:47 +0200

    promena cache size

--- zad8/Config.cs
+++ zad8/Config.cs
@@ -2,7 +2,7 @@ public class ServerConfig
 {
 	public int port = 5050;
 	public int maxConnections = 100;
-	public int maxCacheSize = 10;
+	public int maxCacheSize = 3;
 	public int threadCount = 5;
 	public string rootFolder = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "root");
 }
