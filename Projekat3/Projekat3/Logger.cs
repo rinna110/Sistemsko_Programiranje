4a213e452212c542b2dbe9d4dfe6a3975e135724
katarinaducic <katarina.ducic@elfak.rs>
2026-07-07 18:22:35 +0200

    Logger

---
+++ Projekat3/Projekat3/Logger.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekat3
+{
+    internal class Logger
+    {
+        private static readonly object _lock = new object();
+        public static void Log(string message)
+        {
+            lock (_lock)
+            {
+                Console.WriteLine(DateTime.Now + ": " + message);
+            }
+        }
+
+    }
+}
